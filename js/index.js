22c9c65e5fdfaae0d4a0eab2c4d4eca1c6f677fb
Jose Carlos Robles <jcrobles1989@gmail.com>
2020-03-10 16:04:49 -0700

    wheel done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-const highlightPara = document.getElementsByTagName('p');
+// mouseover
 
+const highlightPara = document.getElementsByTagName('p');
 
 Array.from(highlightPara).forEach (highlightPara => {
 
@@ -11,4 +12,30 @@ highlightPara.addEventListener("mouseover", function( event ) {
         event.target.style.color = "";
       }, 500);
     }, false);
+})
+
+// wheel
+
+function zoom(event) {
+    event.preventDefault();
+  
+    scale += event.deltaY * -0.01;
+  
+    // Restrict scale
+    scale = Math.min(Math.max(.125, scale), 4);
+  
+    // Apply scale transform
+    el.style.transform = `scale(${scale})`;
+  }
+  
+  let scale = 1;
+  const el = document.querySelector('img');
+  el.onwheel = zoom;
+
+  // drag / drop
+
+  const dropPara = document.querySelectorAll('div.text-content, section.content-destination, div.destination')
+
+  Array.from(dropPara).forEach (dropPara => {
+    dropPara.setAttribute('class', 'dropzone');
 })
