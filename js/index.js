ca322db35215a46312e41c180f9a4538ed65ddb5
Jose Carlos Robles <jcrobles1989@gmail.com>
2020-03-12 21:47:29 -0700

    almost done

--- js/index.js
+++ js/index.js
@@ -32,10 +32,80 @@ function zoom(event) {
   const el = document.querySelector('img');
   el.onwheel = zoom;
 
-  // drag / drop
+// drag
 
-  const dropPara = document.querySelectorAll('div.text-content, section.content-destination, div.destination')
+const draggPara = document.querySelectorAll('p')
 
-  Array.from(dropPara).forEach (dropPara => {
-    dropPara.setAttribute('class', 'dropzone');
+Array.from(draggPara).forEach (draggPara => {
+    draggPara.setAttribute('draggable', 'true');
+})
+
+// keydown 
+
+const keyLog = document.createElement('p')
+
+keyLog.setAttribute('id', 'log')
+
+const parentElement = document.querySelector('header.main-navigation');
+
+parentElement.append(keyLog)
+
+document.addEventListener('keydown', logKey);
+
+function logKey(e) {
+  log.textContent += ` ${e.code}`;
+}
+
+keyLog.style.width = '20%';
+
+// mouseenter
+
+const logoChanger = document.querySelector('h1.logo-heading')
+
+logoChanger.addEventListener('mouseenter', event => {
+    logoChanger.style.border = '3px dotted orange';
+    logoChanger.style.transform = 'scale(2)';
+    logoChanger.style.color = 'orange';
+})
+
+// mouseleave
+
+logoChanger.addEventListener('mouseleave', event => {
+    logoChanger.style.border = '';
+    logoChanger.style.transform = '';
+    logoChanger.style.color = '';
+})
+
+// form 
+
+const formContainer = document.querySelector('.intro')
+const form = document.createElement('form')
+form.style.display = 'flex'
+form.style.justifyContent = 'center'
+
+const firstInput = document.createElement('input')
+firstInput.setAttribute('placeholder', 'name')
+firstInput.style.textAlign = 'center'
+form.append(firstInput)
+formContainer.prepend(form)
+
+// load
+
+const body = document.querySelector('body')
+
+body.addEventListener('load', alert('Hello There'))
+
+// resize 
+
+window.addEventListener('resize', () => {
+    alert(`${window.innerWidth}, ${window.innerHeight}
+Resize Works!`)
+})
+
+// dblclick
+
+const imgRotate = document.querySelector('.img-content');
+
+imgRotate.addEventListener('dblclick', () => {
+    imgRotate.style.transform = 'rotate(20deg)'
 })
