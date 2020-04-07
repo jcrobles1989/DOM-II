b62aa933016ac61b7b16e0d96b49d5c507d1011d
Jose Carlos Robles <jcrobles1989@gmail.com>
2020-04-07 15:58:43 -0700

    two event handlers done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+
+const get = (selector) => {
+    document.querySelector(selector)
+}
+
+const getAll = (selector) => {
+   return document.querySelectorAll(selector)
+}
+
+const paraSelector = getAll('p')
+
+paraSelector.forEach( paraSelector => {
+    
+    paraSelector.addEventListener('mouseenter', function(event) {
+        event.target.style.color = 'turquoise'
+    })
+
+    paraSelector.addEventListener('mouseleave', function(event) {
+        event.target.style.color = ''
+    })
+})
+
+
